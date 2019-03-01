c33df613033acce3f647dc5182982a4f0496debf
yanwei <yanwei.cao@advantech.com.cn>
2019-03-01 17:15:17 +0800

    modify pll rate to support HDMI 4K

--- drivers/gpu/drm/rockchip/dw_hdmi-rockchip.c
+++ drivers/gpu/drm/rockchip/dw_hdmi-rockchip.c
@@ -528,9 +528,6 @@ dw_hdmi_rockchip_mode_valid(struct drm_connector *connector,
 	    !drm_mode_is_420(&connector->display_info, mode))
 		return MODE_BAD;
 
-	if (hdmi->dev_type == RK3288_HDMI && mode->clock > 340000)
-		return MODE_BAD;
-
 	/*
 	 * ensure all drm display mode can work, if someone want support more
 	 * resolutions, please limit the possible_crtc, only connect to
